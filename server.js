5fffc25ed39d7d3a5fa84565def7a4ce94deff47
Christian Sheen <christiansheen@MacBook-Pro.local>
2021-01-19 19:26:08 -0500

    building Node.js server w/ routes. initial commit.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+ console.log("GET request from homepage");
+ res.send("Hello GET");
+});
+
+app.post("/", (req, res) => {
+ console.log("POST request from homepage");
+ res.send("Hello POST");
+});
+
+app.delete("/del_user", (req, res) => {
+ console.log("DELETE request from /del_user");
+ res.send("Hello DELETE");
+});
+
+app.get("/list_user", (req, res) => {
+ console.log("GET request from /list_user");
+ res.send("Page Listing");
+});
+
+app.get("/ab*cd", (req, res) => {
+ console.log("GET request from /ab*cd");
+ res.send("Page Pattern Match");
+});
+
+const server = app.listen(3000, () => {
+ const host = server.address().address;
+ const port = server.address().port;
+
+ console.log("Example app listening at http://%s:%s", host, port);
+});
