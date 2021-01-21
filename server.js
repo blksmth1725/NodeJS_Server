a5f8219db32cc7daad9548bd45a5d0f9661647d2
Christian Sheen <christiansheen@MacBook-Pro.local>
2021-01-21 13:06:04 -0500

    deleting fruit count from server.js

--- server.js
+++ server.js
@@ -23,12 +23,3 @@ const server = app.listen(3000, () => {
 
  console.log("Example app listening at http://%s:%s", host, port);
 });
-
-const oranges = ["orange", "orange"];
-const apples = ["just one apple"];
-oranges.forEach((fruit) => {
- console.count(fruit);
-});
-apples.forEach((fruit) => {
- console.count(fruit);
-});
