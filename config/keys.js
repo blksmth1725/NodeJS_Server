e32bc5b70c3fcce32b56d7eee8139b3682cd2944
Christian Sheen <christiansheen@MacBook-Pro.local>
2021-01-27 14:20:58 -0500

    passport setup

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
+ //mongoDB-userPassword: v4Cl7ciztYSjTTWQ
  mongoURI:
   "mongodb+srv://csheen1725:v4Cl7ciztYSjTTWQ@mern-stack-portfolio.c3rec.mongodb.net/MERN-Portfolio?retryWrites=true&w=majority",
- //mongoDB-userPassword: v4Cl7ciztYSjTTWQ
+ secretOrKye: "secret",
 };
