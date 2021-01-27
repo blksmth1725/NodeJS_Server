47b7a8a5fc7ab508ce00c851383bcf66af2d4aea
Christian Sheen <christiansheen@MacBook-Pro.local>
2021-01-27 13:40:04 -0500

    create register endpoint, use "register.js" & "login.js" in validation folder along with User model to check for input validation, followed by hashing password using bycrypt if newUser : "User already exists"

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,3 +10,41 @@ const validateloginInput = require("../../validation/login");
 
 //Load User Model
 const User = require("../../models/User");
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+ //Form validation
+
+ const { errors, isValid } = validateRegistrationInput(req.body);
+
+ //Check Validation
+ if (!isValid) {
+  return res.status(400).json(errors);
+ }
+
+ User.findOne({ email: req.body.email }).then((user) => {
+  if (user) {
+   return res.status(400).json({ email: "Email alreay exists" });
+  } else {
+   const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passqord: req.body.password,
+   });
+
+   //Hash password before saving in database
+   bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+     if (err) throw err;
+     newUser.password = hash;
+     newUser
+      .save()
+      .then((user) => res.json(user))
+      .catch((err) => console.log(err));
+    });
+   });
+  }
+ });
+});
